d5e70f56dae8596cc702bf164a2feb15523284c9
Salman <powerfly@bk.ru>
2020-11-18 15:17:10 +0400

    add new apps in massive

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,7 @@ const apps = [];
 apps [0] = prompt("which king app?", "");
 apps [1] = prompt("which king your app?", "");
 apps [2] = prompt("which king my app?", "");
+apps [2] = prompt("which king ?", "");
 
 document.write(apps);
 
